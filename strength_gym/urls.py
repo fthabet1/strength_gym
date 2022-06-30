a1f115c25f23973373e88e6d609e5323bb0b0cb4
fthabet1 <thabetfuad@gmail.com>
2022-06-29 23:59:52 -0400

    Last Minute Touched to Front Pages

--- strength_gym/urls.py
+++ strength_gym/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("gymDirectory.urls")),
+    path('strength_gym', include("gymDirectory.urls")),
 ]
