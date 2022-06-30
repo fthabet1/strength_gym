5cfb91bcdfdf4af723576a2eb8de890d429016e5
fthabet1 <thabetfuad@gmail.com>
2022-06-29 23:17:19 -0400

    Completed integration of frontend template

--- gymDirectory/urls.py
+++ gymDirectory/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("/about", views.about, name="about"),
+    path("/contact", views.contact, name="contact"),
+    path("/portfolio", views.portfolio, name="portfolio"),
+    path("/pricing", views.pricing, name="pricing"),
+    path("/services", views.services, name="services"),
 ]
--- gymDirectory/views.py
+++ gymDirectory/views.py
@@ -5,3 +5,15 @@ def index(request):
 
 def about(request):
     return render(request, "gymDirectory/about.html", {})
+
+def contact(request):
+    return render(request, "gymDirectory/contact.html", {})
+
+def portfolio(request):
+    return render(request, "gymDirectory/portfolio.html", {})
+
+def pricing(request):
+    return render(request, "gymDirectory/pricing.html", {})
+
+def services(request):
+    return render(request, "gymDirectory/services.html", {})
